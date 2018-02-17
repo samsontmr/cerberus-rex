09165d7546711864da735244946b861828d74384
adamcircle <adamcircle1@gmail.com>
2018-02-17 15:38:18 -0800

    update

--- index.py
+++ index.py
@@ -10,11 +10,13 @@ def process_location_data(json_blob):
     deciphered = json.loads(json_blob)
     write_to_mongo(deciphered)
 
+
 def write_to_mongo(data):
     cli = MongoClient()
     db = cli.pymongo_test
     posts = db.posts
     posts.insert_one(data)
+
 
 def read_from_mongo():
     pass
