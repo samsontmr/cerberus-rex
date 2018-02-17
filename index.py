26db46b073482cfe3aa3df895995c492ecf418db
adamcircle <adamcircle1@gmail.com>
2018-02-17 15:11:32 -0800

    added write_to_db function

--- index.py
+++ index.py
@@ -7,11 +7,14 @@ app = Flask(__name__)
 
 @app.route('/new', method="POST")
 def process_location_data(json_blob):
-    deciphered = json.dump(json_blob)
-    return deciphered
+    deciphered = json.loads(json_blob)
+    write_to_mongo(deciphered)
 
 def write_to_mongo(data):
     cli = MongoClient()
     db = cli.pymongo_test
     posts = db.posts
-    posts.insert_one(json.loads(data))
+    posts.insert_one(data)
+
+def read_from_mongo():
+    pass
