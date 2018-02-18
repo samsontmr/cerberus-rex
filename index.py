7386d665d7bd834acf5b1ca3f13253e0ea15b8e8
adamcircle <adamcircle1@gmail.com>
2018-02-17 20:08:35 -0800

    setup databases and write to databases

--- index.py
+++ index.py
@@ -1,22 +1,66 @@
 from flask import Flask
 import json
-from pymongo import MongoClient
-
+from flask_sqlalchemy import SQLAlchemy
+import uuid
+from twilio.rest import Client
 
 app = Flask(__name__)
-
-@app.route('/new', method="POST")
-def process_location_data(json_blob):
-    deciphered = json.loads(json_blob)
-    write_to_mongo(deciphered)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///main.db'
+db = SQLAlchemy(app)
 
 
-def write_to_mongo(data):
-    cli = MongoClient()
-    db = cli.pymongo_test
-    posts = db.posts
-    posts.insert_one(data)
+class Metadata(db.Model):
+    id = db.Column(db.String, primary_key=True)
+    message_type = db.Column(db.String, unique=False, nullable=False)
+    timestamp = db.Column(db.DateTime, unique=False, nullable=False)
+    latitude = db.Column(db.Float, unique=False, nullable=True)
+    longitude = db.Column(db.Float, unique=False, nullable=True)
+    shooter_nearby = db.Column(db.Boolean, unique=False, nullable=True)
+    medical_requested = db.Column(db.Integer, unique=False, nullable=True)
+
+    def __repr__(self):
+        return '<Metadata-ID: %r>' % self.id
 
 
-def read_from_mongo():
-    pass
+class Audio(db.Model):
+    id = db.Column(db.String, primary_key=True)
+    message_type = db.Column(db.String, unique=False, nullable=False)
+    timestamp = db.Column(db.DateTime, unique=False, nullable=False)
+    audio = db.Column(db.BLOB, unique=False, nullable=True)
+
+    def __repr__(self):
+        return '<Audio-ID: %r>' % self.id
+
+
+db.create_all()
+
+
+@app.route('/new_message', methods=["POST"])
+def process_data(json_blob):
+    data = json.loads(json_blob)
+    if data["uuid"] is None:
+        data["uuid"] = uuid.uuid4()
+    # Process timestamp data
+    try:
+        if data["message_type"] == "Audio":
+            db.session.add(Audio(**data))
+        elif data["message_type"] == "Metadata":
+            db.session.add(Metadata(**data))
+    except:
+        return_packet = {"Success": False}
+    else:
+        return_packet = {
+            "uuid": data["uuid"],
+            "success": True
+        }
+    return json.dumps(return_packet)
+
+
+def initiate_lockdown():
+
+
+
+@app.route('/')
+def process_audio():
+    return "bananas\n", 500
