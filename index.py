55f551a5093e41d74407643323175f51aa11d5b2
adamcircle <adamcircle1@gmail.com>
2018-02-18 01:02:33 -0800

    generate_map in progress

--- index.py
+++ index.py
@@ -4,12 +4,20 @@ import os.path
 import uuid
 from twilio.rest import Client
 import folium
+from folium.plugins import fast_marker_cluster
+import random
+import sys
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets.samples_generator import make_blobs
 
 
 app = Flask(__name__, static_url_path='')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
 db = SQLAlchemy(app)
 root = os.path.join(os.path.dirname(os.path.abspath(__file__)), "ui", "build")
+
 
 class Metadata(db.Model):
     id = db.Column(db.String, primary_key=True)
@@ -37,21 +45,28 @@ class Audio(db.Model):
 
 db.create_all()
 
+def generate_random_data(lat, lon, n_samples, n_centers):
+    centers = np.zeros((n_centers, 2))
+    for i in range(n_centers - 1):
+        dec_lat = (random.random() * 2 - 1) / 100
+        dec_lon = (random.random() * 2 - 1) / 100
+        centers[i][0] = 37.427829 + dec_lat
+        centers[i][1] = -122.170214 + dec_lon
+        centers[-1][0] = lat
+        centers[-1][1] = lon
 
-<<<<<<< Updated upstream
-
-=======
-@app.route('/fetch_data', methods=["POST"])
-def fetch_data
->>>>>>> Stashed changes
+    data, cluster_labels = make_blobs(n_samples=n_samples, centers=centers,
+                                      cluster_std=0.0004, random_state=0)
+    return data, cluster_labels
 
 
 def generate_map():
-    m = folium.Map(location=[37.427829, -122.170214])
+    m = folium.Map(location=[37.427829, -122.170214], zoom_start=13)
+    fast_cluster = fast_marker_cluster([]).add_to(m)
 
     folium.Marker(
         location=[45.3300, -121.6823],
-        popup='Some Other Location',
+        popup='USER{}'.format(Metadata.id),
         icon=folium.Icon(color='red', icon='info-sign')
     ).add_to(m)
     m.save('index.html')
@@ -85,13 +100,15 @@ def process_data():
 def call_police():
     account_sid = "AC5ea2cdc4f220cd56dd5ef910fda8b6d5"
     auth_token = "825cce384001f5b8b469ea5c4e6ef8ed"
-    client = Client(account_sid, auth_token)
-
-    # Start a phone call
-    call = client.calls.create(
-        to="+19145899232",
-        from_="+12019034616",
-        url="https://www.adamcircle.com/emergency_call.xml"
-    )
-
-    return True
+    try:
+        client = Client(account_sid, auth_token)
+        # Start a phone call
+        call = client.calls.create(
+            to="+19145899232",
+            from_="+12019034616",
+            url="https://www.adamcircle.com/emergency_call.xml"
+        )
+    except:
+        return False
+    else:
+        return True
