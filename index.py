fd123f6d5526b85cb3dec2950ce0e8049fd54d24
adamcircle <adamcircle1@gmail.com>
2018-02-18 09:19:40 -0800

    frontend done

--- index.py
+++ index.py
@@ -1,22 +1,48 @@
-from flask import Flask, request, send_from_directory, jsonify, json
+from flask import Flask, request, send_from_directory, jsonify, json, \
+    render_template
 from flask_sqlalchemy import SQLAlchemy
 import os.path
 import uuid
+import logging
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from twilio.rest import Client
 import folium
-from folium.plugins import fast_marker_cluster
+from folium import plugins
+from folium.plugins import FastMarkerCluster
 import random
 import sys
 import math
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+from IPython.display import HTML, display
 from sklearn.datasets.samples_generator import make_blobs
 
+random.seed(1234)
+sns.set(font_scale=1.5)
+
+latitude = 37.427829
+longitude = -122.170214
+counter = 0
 
 app = Flask(__name__, static_url_path='')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
+engine = create_engine('sqlite:///main.db', echo=True)
+Session = sessionmaker(bind = engine)
+session = Session()
+
 db = SQLAlchemy(app)
 root = os.path.join(os.path.dirname(os.path.abspath(__file__)), "ui", "build")
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String, unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Message: %r>' % self.id
 
 
 class Metadata(db.Model):
@@ -45,6 +71,7 @@ class Audio(db.Model):
 
 db.create_all()
 
+
 def generate_random_data(lat, lon, n_samples, n_centers):
     centers = np.zeros((n_centers, 2))
     for i in range(n_centers - 1):
@@ -60,6 +87,55 @@ def generate_random_data(lat, lon, n_samples, n_centers):
     return data, cluster_labels
 
 
+def create_map():
+    X, cluster_labels = generate_random_data(latitude, longitude, 1000, 100)
+
+    df = pd.DataFrame(X)
+    df.to_csv("geo.csv")
+
+    # Loads data from csv into pandas dataframe
+    df = pd.read_csv("geo.csv")
+    df.head(2)
+    print()
+
+    # Stanford coordinates
+    coords = [37.427829, -122.170214]
+
+    # creates an empty map zoomed in Stanford
+    stan_map = folium.Map(location=coords, zoom_start=12)
+
+    # creates a marker cluster called “Crime cluster”
+    marker_cluster = FastMarkerCluster(df.iloc[:, 1:].values.tolist()).add_to(
+        stan_map)
+
+    '''
+    # add a marker for each event, add it to the cluster, not the map
+    for each in df[:100].iterrows():  # for speed purposes delimited data to 15K
+        folium.Marker(location=[each[1]["0"], each[1]["1"]]).add_to(
+            marker_cluster)
+    '''
+
+    display(stan_map)
+    # create HTML interface
+    fg = folium.FeatureGroup(name="Active Shooter")
+
+    props = lambda x: {"fillColor": "green" if
+    x["properties"]["POP2005"] <= 10000000
+    else "orange" if 10000000 < x["properties"]["POP2005"] < 20000000
+    else "red"}
+
+    stan_map.add_child(fg)
+    '''
+    stan_map.add_child(folium.GeoJson(data=open(
+        "world_geojson_from_ogr.json", encoding="utf-8-sig").read(),
+        name="Population",
+        style_function=props))
+    '''
+    stan_map.add_child(folium.LayerControl())
+    stan_map.save(outfile="index.html")
+
+
+'''
 def generate_map():
     m = folium.Map(location=[37.427829, -122.170214], zoom_start=13)
     fast_cluster = fast_marker_cluster([]).add_to(m)
@@ -70,25 +146,61 @@ def generate_map():
         icon=folium.Icon(color='red', icon='info-sign')
     ).add_to(m)
     m.save('index.html')
+'''
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/", methods=["POST"])
+def new_message():
+    global counter
+    data = {"text": request.form["message"], "id": counter}
+    counter += 1
+    print(data)
+    session.add(Message(**data))
+    print(session.query(Message).all())
+    return render_template("index.html", messages=session.query(Message).all())
+
+
+@app.route("/message", methods=["POST"])
+def send_messages():
+    response = app.response_class(
+        response=json.dumps(session.query(Message).all()),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+@app.errorhandler(500)
+def server_error(e):
+    # Log the error and stacktrace.
+    logging.exception("An error occurred during a request.")
+    return "An internal error occurred.", 500
 
 
 @app.route('/new_message', methods=["POST"])
 def process_data():
     data = request.get_json()
     print(data)
-    #police_called = call_police()
+    # police_called = call_police()
     if data["uuid"] is None:
         data["uuid"] = uuid.uuid4()
     # Process timestamp data
-        try:
-            if data["message_type"] == "Audio":
-                db.session.add(Audio(**data))
-            elif data["message_type"] == "Metadata":
-                db.session.add(Metadata(**data))
-        except:
-            return_packet = {"police_called": True,
-                         "success": False,
-                         "uuid": data["uuid"]}
+    try:
+        if data["message_type"] == "Audio":
+            db.session.add(Audio(**data))
+        elif data["message_type"] == "Metadata":
+            db.session.add(Metadata(**data))
+    except:
+        return_packet = {
+            "police_called": True,
+            "success": False,
+            "uuid": data["uuid"]
+        }
     else:
         return_packet = {
             "uuid": data["uuid"],
